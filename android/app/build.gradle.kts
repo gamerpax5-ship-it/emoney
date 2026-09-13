5a3375633badca0f71142aa9b9b68f3c45ce8679
gamersinghji056-lang <gamersinghji056@gmail.com>
2026-09-14 01:13:20 +0530

    Bump digiRupee APK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.loktron.tronpay"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         buildConfigField("String", "WEB_APP_URL", quoted(webAppUrlProvider.get()))
     }
 
